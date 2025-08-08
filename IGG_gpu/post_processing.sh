a7338539cefc07d96de7ad3174e93bdbb73d116b
Teerthal <95438989+Teerthal@users.noreply.github.com>
2025-08-08 11:13:19 -0500

    Delete IGG_gpu directory
    Removing old commits

--- IGG_gpu/post_processing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH -c 26
-#SBATCH -t 0-04:00                  # wall time (D-HH:MM)
-#SBATCH --mem=0
-#SBATCH -o slurm.%j.out             # STDOUT (%j = JobId)
-#SBATCH -e slurm.%j.err             # STDERR (%j = JobId)
-
-time julia -t 26 post_sim.jl
